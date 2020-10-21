4a104b420220ea1dbeaea16465979acc4cc8b6c4
serhiirubets <rubetss@mydigicode.com>
2020-10-21 10:25:50 +0300

    Add game size const

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,7 @@
 import { Game } from './game';
+const GAME_SIZE = 4; // Canvas with 4 X 4 size
 
 const canvas = document.querySelector('#memory-canvas') as HTMLCanvasElement;
-const game = new Game(4, canvas);
+const game = new Game(GAME_SIZE, canvas);
 
 game.start();
