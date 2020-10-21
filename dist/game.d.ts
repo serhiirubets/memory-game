8805c25eb2b15032ecf4a65d008b69ad33c6dffe
serhiirubets <rubetss@mydigicode.com>
2020-10-21 09:56:21 +0300

    Remove dist

--- dist/game.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Game {
-    private cardAmount;
-    private canvas;
-    ctx: CanvasRenderingContext2D;
-    constructor(cardAmount: number, canvas: HTMLCanvasElement);
-    private preInit;
-    start(): void;
-}
